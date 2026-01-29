2218fd567dca951b45936461a93c25fdb1395cf9
Tali Auster <me@tali.network>
2026-01-28 18:31:36 -0800

    chore: init flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        import ./shell.nix { inherit pkgs; };
+
+      packages.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+          pname = "nextbus-sign-server";
+          version = "0.1.0";
+          src = ./.;
+          cargoHash = "sha256-/Eul8Xtgbg6Ofn80sRlzyCcnqI+xI86v+A9zZP0BF3U=";
+        });
+    };
+}
